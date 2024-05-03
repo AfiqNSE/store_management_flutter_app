38910f8a6b598d30ce8c3eaa2b3e241c7d773f9a
AfiqNSE <afiq@nse.com.my>
2024-05-03 15:06:11 +0800

    Create color model

---
+++ lib/models/color_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  Color matteBlack = const Color.fromRGBO(40, 40, 43, 1);
+  Color blueZodiac = const Color.fromRGBO(31, 48, 94, 1);
+  Color tealBlue = const Color.fromRGBO(0, 102, 178, 1);
+  Color yaleBlue = const Color.fromRGBO(0, 66, 140, 1);
+  Color deepSaffron = const Color.fromRGBO(255, 164, 57, 1);
+  Color lightMustard = const Color.fromRGBO(249, 205, 82, 1);
+  Color greyGoose = const Color.fromRGBO(211, 211, 211, 1);
+  Color milkWhite = const Color.fromRGBO(252, 252, 252, 1);
+}
